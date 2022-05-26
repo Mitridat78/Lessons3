6bbddf87fbbc4fd5f4d3fa9abd94b4a8808c7db5
unknown <mitridat78@gmail.com>
2022-05-27 00:57:53 +0300

    new task

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -1,6 +1,10 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("brian", views.brian, name="brian"),
+    path("david", views.david, name='david'),
+    path("<str:name>", views.greet, name="greet")
 ]
--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!!!")
+    return render(request, "hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian")
+
+def david(request):
+    return HttpResponse("Hello, David")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+        })
